ea6cc4b187838a11b18ab659e776c16a2b32f308
Jim Burnham <jim.the.steam.clown@gmail.com>
2023-02-22 09:52:20 -0800

    Create Py-Ch3-FUNCTIONS-Lab-min-max.py

---
+++ Py-Ch3-FUNCTIONS-Lab-min-max.py
@@ -0,0 +1,27 @@
+# Functions Introduction
+print('''Functions Introduction
+There is a big list of built-in functions here:
+https://www.w3schools.com/python/python_ref_functions.asp
+''')      
+print('''
+Challenge #1
+Run, then Edit the for loop in the template example:
+      
+Using some built-in Functions
+- Why is "s" bigger thatn "S"?
+- Why does it look like the "smallest" did not print anything?
+- Is it what you expected?
+      ''')
+my_string = 'STEAM Is Cool'
+biggest = max(my_string)
+print('The biggest character in the \nstring "STEAM Is Cool" is',biggest)
+smallest = min(my_string)
+print('The smallest character in the \nstring "STEAM Is Cool" is',smallest)
+# print('Done with Challenge 1')
+
+
+print('''
+Challenge #2
+Update the code above to ask the user for a long sentance 
+string and assign it to the variable 'my_string'
+      ''') 
