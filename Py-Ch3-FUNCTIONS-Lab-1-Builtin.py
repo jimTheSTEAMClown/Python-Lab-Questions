feb5c69a00d4114dab07c39f240f0cb41fac179d
Jim Burnham <jim.the.steam.clown@gmail.com>
2023-02-22 16:17:25 -0800

    Create Py-Ch3-FUNCTIONS-Lab-1-Builtin.py

---
+++ Py-Ch3-FUNCTIONS-Lab-1-Builtin.py
@@ -0,0 +1,20 @@
+# Python Lab
+print('Playing with Builtin Python Functions')
+student_name = "firstName lastName"
+program_creation_date = "mm/dd/yyyy"
+print(student_name)
+print(program_creation_date)
+print('''Functions Introduction
+There is a big list of built-in functions here:
+https://www.w3schools.com/python/python_ref_functions.asp
+''')     
+print('''This Lab is about using Python Builtin Functions. Try and show code using the following functions, then pick 4 more to try. ...
+- print()
+- input('enter something here: ')
+- int()
+- type()
+- abs()
+- len()
+
+My code follows:''')
+# update the 'student_name' and 'program_creation_date' variables and add your code here:
