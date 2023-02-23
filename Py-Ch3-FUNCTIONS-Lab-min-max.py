fdd2e0155430d05b33f91588f641cde6d854e82d
Jim Burnham <jim.the.steam.clown@gmail.com>
2023-02-22 16:16:08 -0800

    Update Py-Ch3-FUNCTIONS-Lab-min-max.py

--- Py-Ch3-FUNCTIONS-Lab-min-max.py
+++ Py-Ch3-FUNCTIONS-Lab-min-max.py
@@ -1,18 +1,36 @@
 # Functions Introduction
+# Python Lab - Functions - min() max()
+print('Playing with Builtin Python Functions')
+print('min() max()')
+student_name = "firstName lastName"
+program_creation_date = "mm/dd/yyyy"
+print(student_name)
+print(program_creation_date)
 print('''Functions Introduction
 There is a big list of built-in functions here:
 https://www.w3schools.com/python/python_ref_functions.asp
-''')      
+''') 
+print('''This Lab is about using Python Builtin Functions min() and max().
+Try and show code using the following functions:
+- max()
+- min()
+
+My code follows:''')
 print('''
 Challenge #1
-Run, then Edit the for loop in the template example:
+Run, then make sure you understand why is "s" bigger 
+than "S" and why a blank space is printed:
       
-Using some built-in Functions
-- Why is "s" bigger thatn "S"?
+Using the built-in Functions max() & min()
+- Why is "s" bigger than "S"?
 - Why does it look like the "smallest" did not print anything?
-- Is it what you expected?
+- Is it what you expected? Can you explain it?
+Check out the link to the Extended ASCII Table Codes
+https://www.ascii-code.com/
+
       ''')
 my_string = 'STEAM Is Cool'
+print('The variable "my_string" = ',my_string)
 biggest = max(my_string)
 print('The biggest character in the \nstring "STEAM Is Cool" is',biggest)
 smallest = min(my_string)
@@ -22,6 +40,10 @@ print('The smallest character in the \nstring "STEAM Is Cool" is',smallest)
 
 print('''
 Challenge #2
-Update the code above to ask the user for a long sentance 
-string and assign it to the variable 'my_string'
-      ''') 
+Update the code above to use the input() function and 
+ask the user for a long sentence string and assign 
+it to the variable 'my_string'
+
+With this new user input string, see what the min() and 
+max() character is.
+      ''')    
